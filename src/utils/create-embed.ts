cb7f065ae0206adbb003718e79108ec667eb4578
sct <sct@users.noreply.github.com>
2020-07-04 20:28:14 +0900

    Handle showing waiting players for groups (#13)

--- src/utils/create-embed.ts
+++ src/utils/create-embed.ts
@@ -4,11 +4,28 @@ import { MessageEmbed, EmbedField } from 'discord.js';
 export const createEmbed = (events: Event[]): MessageEmbed => {
   const fields: EmbedField[] = [];
   events.forEach((event) => {
-    const players = event.players.map((p) => p.name).join(', ');
+    const players = event.players
+      .slice(0, 5)
+      .map((p) => p.name)
+      .join(', ');
+
+    let waitingPlayers;
+    if (event.players.length > 5) {
+      waitingPlayers = event.players
+        .slice(5)
+        .map((p) => p.name)
+        .join(', ');
+    }
+
+    const totalPlayers = event.players.length > 5 ? 5 : event.players.length;
+    const totalWaiting =
+      event.players.length <= 5 ? 0 : event.players.length - 5;
 
     fields.push({
       name: `${event.emoji} ${event.name}`,
-      value: `${event.players.length}/5 - ${players}`,
+      value: `${totalPlayers}/5 (${totalWaiting} waiting)  - ${players}${
+        waitingPlayers ? ` (${waitingPlayers})` : ''
+      }`,
       inline: false,
     });
   });
